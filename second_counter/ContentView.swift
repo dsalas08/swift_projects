f3e7d3c223061aacdf0ea1fe7a29380a86575cbf
Dan S <dansalas@mac.com>
2023-07-26 17:43:36 -0700

    Initial Commit

---
+++ second_counter/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  second_counter
+//
+//  Created by Daniel Salas on 7/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var count = 0
+    
+    
+    
+    var body: some View {
+        
+        VStack {
+            Text("People: \(String(count))")
+                .font(.largeTitle)
+                .padding()
+            Button(action: {
+                count += 1
+                print(count)
+            }) {
+                Text("Up")
+            }
+            Button(action: {
+                count += -1
+                print(count)
+            }) {
+                Text("Remove")
+            }
+            .padding()
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ second_counter/second_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  second_counterApp.swift
+//  second_counter
+//
+//  Created by Daniel Salas on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct second_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
